c33de1f60d2cd70fa20d6d9620f23f279093f36c
Tristan Mouritsen <tmouritsen57@gmail.com>
2021-11-03 20:57:56 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Wallet extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Virtual Wallet App</h1>
+      </div>
+    );
+  }
+}
